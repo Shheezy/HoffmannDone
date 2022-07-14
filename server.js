9cefa583ff508af6f588d6c6c27cb3f9a9ac2907
Shheezy <simon.roland15@gmail.com>
2022-07-14 21:42:21 +0200

    v1.0.1

---
+++ server.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+
+const tmi = require('tmi.js');
+
+const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
+
+const commands = {
+  website: {
+    response: 'https://discord.teglastream.hu'
+  },
+  upvote: {
+    response: (user) => `User ${user} was just upvoted!`
+  }
+}
+
+
+
+const client = new tmi.Client({
+  connection: {
+    reconnect: true
+  },
+	channels: [ 'hoffmann_bot' ],
+  identity: {
+		username: process.env.TWITCH_BOT_USERNAME,
+		password: process.env.TWITCH_OAUTH_TOKEN
+	},
+});
+
+
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+  const isNotBot = tags.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME;
+
+  if ( !isNotBot ) return;
+
+  const [raw, command, argument] = message.match(regexpCommand);
+
+  const { response } = commands[command] || {};
+
+  if ( typeof response === 'function' ) {
+    client.say(channel, response(tags.username));
+  } else if ( typeof response === 'string') {
+    client.say(channel, response);
+  }
+	// "Alca: Hello, World!"
+	console.log(`${tags['display-name']}: ${message}`);
+
+  
+});
